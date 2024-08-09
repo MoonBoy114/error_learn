9ab85801fa9e78c3aff0510a909edc11c00e39c6
JKMasterMind <elvis.chubuckchiev@bk.ru>
2024-08-09 09:29:42 +0300

    learn_error has completed

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,39 @@
+
+use std::error::Error;
 use std::io::{self, Read};
-use std::fs::File;
+use std::fs::{self, File};
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let f =File::open("hello.txt");
-
-    let mut f = match f {
-        Ok(file) => file,
-        Err(error) => return Err(error),
-    };
-
-    let mut s = String::new();
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
-    }
+    // let mut s = String::new();
+    // File::open("hello.txt")?.read_to_string(&mut s)?;
+    // Ok(s)
+    fs::read_to_string("hello.txt")
 }
 
+pub struct Guess {
+    value: i32,
+}
 
-fn main() {
+impl Guess {
+    pub fn find_value(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Секретное число в пределах от 1 до 100, получено {}", value);
+
+        }
+
+        Guess { value }
+
+    }
+
+        pub fn value(&self) -> i32 {
+            self.value
+        }
+    }
+
+
+
+ fn main() //-> Result<(), Box<dyn Error>>// 
+ {
     // let v = vec![1,2,3];
     // // let f = File::open("hello.txt").unwrap_or_else(|error| {
     // //     if error.kind() == ErrorKind::NotFound {
@@ -37,5 +53,8 @@ fn main() {
     // //         },
     // //         other_error => panic!("Проблема с открытием файла: {:?}", other_error),
     // //     }
-    // let f = File::open("hello.txt").expect("Не удалось открыть файл hello.txt");
+    // let f = File::open("hello.txt").expect("Не удалось открыть файл hello.txt");                             
+    // let mut f = File::open("hello.txt")?;
+    // Ok(())
+    
 }
